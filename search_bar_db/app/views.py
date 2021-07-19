69e481380176ab76b41d3e677bd039907d7195f7
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-07-19 11:42:52 +0530

    Add files via upload

--- search_bar_db/app/views.py
+++ search_bar_db/app/views.py
@@ -1,15 +1,28 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
+from django.shortcuts import render,HttpResponse
+from .models import words
+from django.contrib import messages
 
 
 
-urlpatterns = [
 
-    path('admin/', admin.site.urls),
-    path('',views.home,name="home"),
-    path('search',views.search,name="search"),
-    path('own/<id>',views.own)
+# Create your views here.
+def home(request):
+    letters = words.objects.all()
+
+    return render(request, 'home.html', {'letters': letters})
 
 
-]
+def search(request):
+    if request.method=='POST':
+        searched= request.POST['searched']
+        letters=words.objects.filter(regular_word__contains=searched)
+        return render(request, 'search.html',{'searched':searched,'letters':letters})
+    else:
+        return render(request, 'search.html')
+
+def own(request,id):
+    letters = words.objects.get(id=id)
+    return render(request,'own.html',{'letters':letters})
+
+
+
